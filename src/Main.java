a01127f96b973d71c3eb16f4f194fa8e0b9a5ffb
ArtemPavl <artem.pavlenko.93@inbox.ru>
2022-09-19 23:12:55 +0300

    homework3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
+        var weightFirstBoxer =  78.2;
+        var weightSecondBoxer =  82.7;
+        var weightTwoBoxers = weightFirstBoxer + weightSecondBoxer;
+        System.out.println("Вес обоих баксеров " + weightTwoBoxers + " кг!");
 
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+        var weightDifference = weightSecondBoxer - weightFirstBoxer;
+        System.out.println("Разница в весе " + weightDifference + " кг!");
+
+        weightDifference = weightSecondBoxer % weightFirstBoxer;
+        System.out.println("Разница в весе " + weightDifference + " кг!");
+
+        var timeWork = 64;
+        var timeOneWorker = 8;
+        var workers = timeWork / timeOneWorker;
+        System.out.println("Всего работников в компании – " + workers + " человек");
+
+        var workersAdd = 94;
+        workers = workers + workersAdd;
+        timeWork = timeOneWorker * workers;
+        System.out.println("Если в компании работает " + workers + " человека, то всего " + timeWork + " часов работы может быть поделено между сотрудниками");
     }
 }
